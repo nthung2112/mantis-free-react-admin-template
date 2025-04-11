6ec5e8bcef167ddf3916371afe9f1825354de48e
Hung Nguyen <nthung2112@gmail.com>
2025-04-11 13:22:37 +0700

    Remove files unused

--- src/index.tsx
+++ src/index.tsx
@@ -33,7 +33,6 @@ import '@fontsource/public-sans/700.css';
 
 // project imports
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 
@@ -46,8 +45,3 @@ const root = createRoot(container);
 // ==============================|| MAIN - REACT DOM RENDER ||============================== //
 
 root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
